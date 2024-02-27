50e54f0cb8d37b09d9366c3065d7577766346086
ThomasFBN <thni0004@stud.kea.dk>
2024-02-27 13:27:46 +0100

    Initial commit

---
+++ src/main/java/com/example/githubactionspringboot/GithubActionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionspringboot/GithubActionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
